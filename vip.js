e7c4cb7a5d710e47ddb9d3b3a8c6fec3a7cd244c
Digbaro <digbaro95@gmail.com>
2026-07-08 04:17:28 +0100

    first commit

---
+++ vip.js
@@ -0,0 +1,18 @@
+
+let vipList = ["Tunde", "Sola", "AY", "Femi", "Tobi"];
+let customerName = prompt("What is your name?");
+
+if (vipList.includes(customerName)){
+    let position = vipList.indexOf(customerName);
+    alert("Welcome VIP! You are guest #"+ position + ". Free Entry.")
+} else {
+    let ticketNumber = prompt("what is your Ticket Number?; ")
+    if (ticketNumber % 3 === 0 && ticketNumber && 5 === 0){
+        console.log("Free Ticket.");
+    }
+    else if (ticketNumber % 3 === 0){
+        console.log("50% off");
+    } else {
+        console.log("Full Price.")
+    }
+}
